ab2a9de86ff66bccf792b7eab006fe06790803c0
xiyuan-qin <88170667@qq.com>
2025-03-21 11:12:40 +0800

    outer sort with min lose tree

--- data_structure_course_design/Ex2/minLoseTree.cpp
+++ data_structure_course_design/Ex2/minLoseTree.cpp
@@ -1,20 +1,222 @@
 #include "minLoseTree.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <climits>
+
+// 初始化数据到磁盘文件
+void init(Variable &va) {
+    std::cout << "请输入您想要的模拟磁盘位置，接下来的操作都是在当前路径下进行，输入样例：/Users/username/Desktop/text/" << std::endl;
+    std::cout << "请输入："; 
+    std::cin >> va.path; 
+    va.file = va.path + va.fileName;
+    
+    std::cout << "请输入您想要在磁盘中初始化数字的个数，这些数字将用于模拟外排序过程："; 
+    std::cin >> va.n;
+    
+    std::cout << "请输入缓冲区大小（此处为缓冲区能存储数字的个数）："; 
+    std::cin >> va.m;
+    
+    std::cout << "请问您是否想要将原始数据，顺串，最终数据显示在控制台中('y' or 'n')："; 
+    std::cin >> va.ch1; 
+    std::cout << std::endl;
+
+    std::ofstream fout1(va.file);
+    if (!fout1.is_open()) {
+        std::cout << "无法打开指定磁盘文件，无法初始化磁盘" << std::endl;
+        exit(0);
+    }
+    
+    if (va.ch1 == 'y') 
+        std::cout << "原始磁盘文件有：" << std::endl;
+    
+    srand(time(nullptr));
+    for (int i = 1; i <= va.n; i++) {
+        int x = rand() % 1000 + 1;
+        fout1 << x << ' ';
+        if (va.ch1 == 'y') 
+            std::cout << x << ' ';
+    }
+    
+    if (va.ch1 == 'y') 
+        std::cout << std::endl << std::endl;
+    
+    fout1.close();
+}
+
+// 构造顺串
+void SequentialStringConstruction(Variable &va, SequentialStringPlayer* ssplayer) {
+    std::ifstream fin1(va.file);
+    if (!fin1.is_open()) {
+        std::cout << "无法打开指定磁盘文件，无法进行顺串构造" << std::endl;
+        exit(0);
+    }
+    
+    // 将数据读入缓冲区，进行顺串构造
+    for (int i = 1; i <= va.m; i++) {
+        fin1 >> ssplayer[i].key;
+        ssplayer[i].id = 1;
+        va.visitDiskTime++; // 访存次数+1
+    }
+    
+    MinimumLoserTree<SequentialStringPlayer> sstree(ssplayer, va.m);
+    int num = 0;
+    
+    for (int i = 1; i <= va.n; i++) {
+        // 依次从磁盘中取出元素，放入顺串生成树
+        if (!(fin1 >> num)) 
+            num = INT_MIN;
+        else 
+            va.visitDiskTime++;
+
+        SequentialStringPlayer tempwinner = ssplayer[sstree.getTheWinner()];
+        SequentialStringPlayer tempnum; 
+        tempnum.key = num;
+
+        if (num >= tempwinner.key) //局部决策：算法不会全局比较所有顺串的末尾元素，而是只与刚输出的元素比较
+            tempnum.id = tempwinner.id;
+        else 
+            tempnum.id = (num == INT_MIN) ? INT_MAX : tempwinner.id + 1;
+
+        sstree.rePlay(sstree.getTheWinner(), tempnum);
+
+        std::string smallfile = va.path + "smallfile" + std::to_string(tempwinner.id) + ".txt";
+        va.numberOfDisk = std::max(va.numberOfDisk, tempwinner.id);
+        std::ofstream fout2(smallfile, std::ios::app);
+        fout2 << tempwinner.key << ' ';
+
+        fout2.close();
+        va.visitDiskTime++;
+    }
+    
+    fin1.close();
+    std::cout << "顺串分配完毕，生成" << va.numberOfDisk << "个顺串，顺串文件见您当前路径下出现的新文件" << std::endl;
+    
+    if (va.ch1 == 'y') {
+        std::cout << "我们将这些数据在这里显示如下：" << std::endl;
+        for (int i = 1; i <= va.numberOfDisk; i++) {
+            std::string smallfile = va.path + "smallfile" + std::to_string(i) + ".txt";
+            std::ifstream finx(smallfile);
+            int tempx = 0;
+            std::cout << "smallfile" << i << ".txt: ";
+            while (finx >> tempx)
+                std::cout << tempx << ' ';
+            std::cout << std::endl;
+            finx.close();
+        }
+    }
+}
+
+// 生成最终结果
+void GenerateTheFinalResult(Variable &va) {
+    std::cout << std::endl << "请问是否将最终排序结果放入原文件，如果否，则程序将新建一个Disk2.txt文件并放入此文件中（输入'y'或'n'代表'是'与'否')："; 
+    std::cin >> va.ch;
+    
+    std::string newFile;
+    if (va.ch == 'y') 
+        newFile = va.file;
+    else 
+        newFile = va.path + "Disk2.txt";
+
+    std::ofstream fout3(newFile);
+
+    if (va.numberOfDisk == 1) { // 只生成了一个顺串文件，直接将其加入目标文件
+        std::string smallfile = va.path + "smallfile" + std::to_string(1) + ".txt";
+        std::ifstream fin4(smallfile);
+        int tempnumber;
+        while (fin4 >> tempnumber) {
+            fout3 << tempnumber << ' ';
+            va.visitDiskTime += 2;
+        }
+        fout3.close();
+        fin4.close();
+        std::cout << "由于只生成了1个顺串，直接将此结果放入目标文件中，磁盘访存次数为" << va.visitDiskTime << "次，原因是每个数据都读写4次" << std::endl;
+        
+        if (va.ch1 == 'y') {
+            std::cout << "最终外排序结果如下：" << std::endl;
+            std::ifstream finy(newFile);
+            int tempy;
+            while (finy >> tempy)
+                std::cout << tempy << ' ';
+            std::cout << std::endl;
+            finy.close();
+        }
+        return;
+    }
+
+    // 初始化K路归并
+    int* dplayer = new int[va.numberOfDisk + 10]; // 初始化队列
+    int* pointer = new int[va.numberOfDisk + 10]; // 各个小磁盘文件的指针
+    
+    for (int i = 1; i <= va.numberOfDisk; i++) {
+        std::string smallfile = va.path + "smallfile" + std::to_string(i) + ".txt";
+        std::ifstream fin2(smallfile);
+        fin2 >> dplayer[i];
+        pointer[i] = fin2.tellg();
+        fin2.close();
+    }
+    
+    MinimumLoserTree<int> dtree(dplayer, va.numberOfDisk);
+    int cnt = 0;
+    
+    while (cnt < va.n) {
+        cnt++;
+        int temp = dtree.getTheWinner();
+        int tempwinner = dplayer[temp];
+        fout3 << tempwinner << ' ';
+        va.visitDiskTime++;
+        
+        std::string smallfile = va.path + "smallfile" + std::to_string(temp) + ".txt";
+        std::ifstream fin3(smallfile);
+        fin3.clear(); 
+        fin3.seekg(pointer[temp] + 1);
+
+        int tempnum;
+        if (pointer[temp] + 1 == 0) 
+            tempnum = INT_MAX;
+        else {
+            fin3 >> tempnum;
+            pointer[temp] = fin3.tellg();
+            if (pointer[temp] + 1 == 0) 
+                tempnum = INT_MAX;
+        }
+        
+        va.visitDiskTime++;
+        dtree.rePlay(temp, tempnum);
+        fin3.close();
+    }
+    
+    fout3.close();
+    delete[] dplayer;
+    delete[] pointer;
+    
+    std::cout << std::endl;
+    std::cout << "将这些文件进行" << va.numberOfDisk << "路归并，已经结果存入到目标文件中。磁盘访存次数为" << va.visitDiskTime << "次，原因是每个数据都读写4次" << std::endl;
+    
+    if (va.ch1 == 'y') {
+        std::cout << "最终外排序结果如下：" << std::endl;
+        std::ifstream finy(newFile);
+        int tempy;
+        while (finy >> tempy)
+            std::cout << tempy << ' ';
+        std::cout << std::endl;
+        finy.close();
+    }
+}
 
 int main() {
-    std::cout << "程序开始执行..." << std::endl;
-    
-    std::vector<int> values = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-    std::cout << "输入数组大小: " << values.size() << std::endl;
-    
-    std::cout << "创建败者树..." << std::endl;
-    LoserTree<int> tree(values);
-    
-    std::cout << "打印树结构:" << std::endl;
-    tree.printTree();
-    
-    std::cout << "Winner index: " << tree.WinnerIndex() << std::endl;
-    std::cout << "Winner value: " << tree.WinnerValue() << std::endl;
+    Variable va;
 
+    init(va); // 初始化，生成随机磁盘文件
+
+    SequentialStringPlayer* ssplayer = new SequentialStringPlayer[va.n + 10];
+
+    SequentialStringConstruction(va, ssplayer); // 构造顺串
+
+    GenerateTheFinalResult(va); // 生成最终结果
+
+    delete[] ssplayer;
     return 0;
 }
--- data_structure_course_design/Ex2/minLoseTree.h
+++ data_structure_course_design/Ex2/minLoseTree.h
@@ -2,97 +2,203 @@
 #define MIN_LOSER_TREE_H
 
 #include <vector>
-#include <iostream> // 添加用于调试输出
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <climits>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
 
-template <class T>
-class LoserTree {
-private:
-    std::vector<T> extValues;  // 外部结点值
-    std::vector<int> loserTree; // 败者树
-    int k;                     // 外部结点数
-    int winner;                // 冠军（赢者）
-
+// 系统变量结构体
+class Variable {
 public:
-    LoserTree(const std::vector<T>& values);
-    void build();
-    void adjust(int i);
-    int WinnerIndex() const;
-    T WinnerValue() const;
-    void printTree() const; // 添加打印树的方法用于调试
+    Variable() {
+        visitDiskTime = 0;
+        n = 0; m = 0;
+        numberOfDisk = 0;
+    }
+    std::string path;        // 磁盘的位置
+    std::string fileName = "Disk.txt"; // 磁盘文件名称
+    std::string file;        // 最终完整路径
+    int visitDiskTime;       // 访问磁盘次数
+    int n;                   // 数据总数
+    int m;                   // 缓冲区大小
+    int numberOfDisk;        // 小文件数量
+    char ch1, ch;            // 用户选择
 };
 
-// 模板类实现
+// 顺串构造器结构体
+struct SequentialStringPlayer {
+    int id, key;
+    bool operator<=(SequentialStringPlayer &p) {
+        return (id != p.id) ? (id < p.id) : (key < p.key);
+    }
+};
+
+// 败者树基类接口
 template <class T>
-LoserTree<T>::LoserTree(const std::vector<T>& values) : extValues(values) {
-    k = values.size();
-    if (k == 0) {
-        winner = -1;
-        return; // 防止空数组
+class loserTree {
+public:
+    virtual ~loserTree() {}
+    virtual void initialize(T* thePlayer, int number) = 0;
+    virtual int getTheWinner() const = 0;
+    virtual void rePlay(int thePlayer, T newvalue) = 0;
+};
+
+// 最小败者树实现
+template <class T>
+class MinimumLoserTree : public loserTree<T> {
+public:
+    MinimumLoserTree(T* thePlayer = nullptr, int theNumberOfPlayers = 0) {
+        tree = nullptr; 
+        advance = nullptr;
+        initialize(thePlayer, theNumberOfPlayers);
     }
     
-    loserTree.resize(k - 1, -1);
-    winner = 0; // 初始化胜者
-    build();
+    ~MinimumLoserTree() {
+        delete[] tree;
+        delete[] advance;
+    }
+    
+    void initialize(T* thePlayer, int theNumberOfPlayers);  // 初始化
+    int getTheWinner() const { return tree[0]; }            // 输出当前的赢者
+    void rePlay(int thePlayer, T newvalue);                 // 重构
+    
+private:
+    int numberOfPlayers;
+    int* tree;    // 记录内部结点，tree[0]是最终的赢者下标
+    int* advance; // 记录比赛晋级的成员
+    T* player;    // 参与比赛的元素
+    int lowExt;   // 最底层外部结点的个数,2*(n-s)
+    int offset;   // 2*s-1
+    
+    void play(int p, int leftChild, int rightChild);
+    int winner(int x, int y) { return player[x] <= player[y] ? x : y; }  // 返回更小的元素下标
+    int loser(int x, int y) { return player[x] <= player[y] ? y : x; }   // 返回更大的元素下标
+};
+
+// 初始化树结构
+template <class T>
+void MinimumLoserTree<T>::initialize(T* thePlayer, int theNumberOfPlayers) {
+    int n = theNumberOfPlayers;
+    if (n < 2) {
+        std::cout << "Error! the number must >= 2" << std::endl;
+        return;
+    }
+    
+    player = thePlayer;
+    numberOfPlayers = n;
+    delete[] tree; 
+    delete[] advance;
+    tree = new int[n + 1];
+    advance = new int[n + 1];
+
+    // 计算s值 (s=2^log(n-1)-1)
+    int s; 
+    for (s = 1; 2 * s <= n - 1; s += s);
+    
+    lowExt = 2 * (n - s); 
+    offset = 2 * s - 1;
+
+    // 最下面一层开始比赛
+    for (int i = 2; i <= lowExt; i += 2)
+        play((i + offset) / 2, i - 1, i);
+
+    int temp = 0;
+    if (n % 2 == 1) {  // 奇数个结点
+        play(n / 2, advance[n - 1], lowExt + 1);
+        temp = lowExt + 3;
+    }
+    else temp = lowExt + 2;  // 偶数个结点
+
+    // 处理次下层
+    for (int i = temp; i <= n; i += 2)
+        play((i - lowExt + n - 1) / 2, i - 1, i);
+
+    tree[0] = advance[1];  // tree[0]是最终的赢者
 }
 
+// 比赛过程
 template <class T>
-void LoserTree<T>::build() {
-    // 初始化loserTree数组
-    for (int i = 0; i < k - 1; ++i) {
-        loserTree[i] = -1;
-    }
-    
-    // 从最后一个外部节点开始调整
-    for (int i = k - 1; i >= 0; --i) {
-        adjust(i);
+void MinimumLoserTree<T>::play(int p, int leftChild, int rightChild) {
+    // tree结点存储相对较大的值，也就是这场比赛的输者
+    tree[p] = loser(leftChild, rightChild);
+    // advance结点存储相对较小的值，也就是这场比赛的晋级者
+    advance[p] = winner(leftChild, rightChild);
+
+    while (p % 2 == 1 && p > 1) {
+        tree[p / 2] = loser(advance[p - 1], advance[p]);
+        advance[p / 2] = winner(advance[p - 1], advance[p]);
+        p /= 2;  // 向上搜索
     }
 }
 
+// 重构树，当一个选手改变值时
 template <class T>
-void LoserTree<T>::adjust(int i) {
-    if (k <= 0) return;
-    
-    int parent = (i + k - 1) / 2;  // 计算父节点位置
-    
-    while (parent > 0) {
-        // 如果尚未有败者，或者当前节点值大于已存储的败者，则当前节点成为新败者
-        if (loserTree[parent] == -1 || extValues[i] > extValues[loserTree[parent]]) {
-            std::swap(i, loserTree[parent]);
+void MinimumLoserTree<T>::rePlay(int thePlayer, T newvalue) {
+    int n = numberOfPlayers;
+    if (thePlayer <= 0 || thePlayer > n) {
+        std::cout << "Error! the number must >0 & <=n" << std::endl;
+        return;
+    }
+
+    player[thePlayer] = newvalue;
+
+    int matchNode,     // 将要比赛的场次
+        leftChild,     // 比赛结点的左儿子
+        rightChild;    // 比赛结点的右儿子
+
+    if (thePlayer <= lowExt) {  // 如果要比赛的结点在最下层
+        matchNode = (offset + thePlayer) / 2;
+        leftChild = 2 * matchNode - offset;
+        rightChild = leftChild + 1;
+    }
+    else {  // 要比赛的结点在次下层
+        matchNode = (thePlayer - lowExt + n - 1) / 2;
+        if (2 * matchNode == n - 1) {  // 特殊情况，其中一方是晋级后的人
+            leftChild = advance[2 * matchNode];
+            rightChild = thePlayer;
         }
-        parent /= 2;  // 向上移动到树的下一层
+        else {
+            leftChild = 2 * matchNode - n + 1 + lowExt;  // 这个操作是因为上面matchNode计算中/2取整了
+            rightChild = leftChild + 1;
+        }
     }
-    
-    // 处理根节点（即全局胜者的对手）
-    if (loserTree[0] == -1 || extValues[i] > extValues[loserTree[0]]) {
-        std::swap(i, loserTree[0]);
+
+    // 比赛重构，分两种情况
+    if (thePlayer == tree[0]) {  // 当要重构的点是上一场比赛的赢家
+        for (; matchNode >= 1; matchNode /= 2) {
+            int oldLoserNode = tree[matchNode];  // 上一场比赛的输者
+            tree[matchNode] = loser(oldLoserNode, thePlayer);
+            advance[matchNode] = winner(oldLoserNode, thePlayer);
+            thePlayer = advance[matchNode];
+        }
     }
-    
-    winner = loserTree[0];  // 根节点存储的是全局胜者的对手（即最小值）
+    else {  // 其他情况重构
+        tree[matchNode] = loser(leftChild, rightChild);
+        advance[matchNode] = winner(leftChild, rightChild);
+        if (matchNode == n - 1 && n % 2 == 1) {  // 特殊情况
+            matchNode /= 2;
+            tree[matchNode] = loser(advance[n - 1], lowExt + 1);
+            advance[matchNode] = winner(advance[n - 1], lowExt + 1);
+        }
+        matchNode /= 2;
+        for (; matchNode >= 1; matchNode /= 2) {
+            tree[matchNode] = loser(advance[matchNode * 2], advance[matchNode * 2 + 1]);
+            advance[matchNode] = winner(advance[matchNode * 2], advance[matchNode * 2 + 1]);
+        }
+    }
+    tree[0] = advance[1];  // 最终胜者
 }
 
-template <class T>
-int LoserTree<T>::WinnerIndex() const {
-    return winner;
-}
+// 初始化数据到磁盘文件
+void init(Variable &va);
 
-template <class T>
-T LoserTree<T>::WinnerValue() const {
-    if (k > 0 && winner >= 0 && winner < k)
-        return extValues[winner];
-    return T(); // 返回默认值
-}
+// 构造顺串
+void SequentialStringConstruction(Variable &va, SequentialStringPlayer* ssplayer);
 
-template <class T>
-void LoserTree<T>::printTree() const {
-    std::cout << "External values: ";
-    for (size_t i = 0; i < extValues.size(); ++i) {
-        std::cout << extValues[i] << " ";
-    }
-    std::cout << "\nLoser tree: ";
-    for (size_t i = 0; i < loserTree.size(); ++i) {
-        std::cout << loserTree[i] << " ";
-    }
-    std::cout << "\nWinner index: " << winner << std::endl;
-}
+// 生成最终结果
+void GenerateTheFinalResult(Variable &va);
 
 #endif // MIN_LOSER_TREE_H
